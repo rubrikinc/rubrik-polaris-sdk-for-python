619271808fa4c2bf25b047177f36cb576b1f8a8d
drew-russell <drew.russell@rubrik.com>
2021-04-20 16:00:49 -0500

    add get_cdm_cluster_connection_status

---
+++ sample/rubrik_polaris/get_cdm_cluster_connection_status.py
@@ -0,0 +1,12 @@
+from rubrik_polaris import PolarisClient
+
+
+domain = 'my-company'
+username = 'john.doe@example.com'
+password = 's3cr3tP_a55w0R)'
+cluster_id = '602909f2-q330-4da9-9389-911d66abb529'
+
+
+client = PolarisClient(domain, username, password, insecure=True)
+
+cluster_status = client.get_cdm_cluster_connection_status(cluster_id)
