0973f2655fda255f82607add20d8c77280845a28
Pontus Leitzler <leitzler@users.noreply.github.com>
2022-02-24 15:20:04 +0100

    Fix existing unit tests (#49)
    Also removes one of the validation tests for raw query as it is
    obsolete. The raw requests aren't validated client side by design.

--- tests/rubrik_polaris/connection_test.py
+++ tests/rubrik_polaris/connection_test.py
@@ -7,21 +7,6 @@ from conftest import util_load_json, BASE_URL
 QUERY_NAME = "cdm_cluster_location"
 
 
-@pytest.mark.parametrize("query_name, raw_query, variables, timeout, err_msg", [
-    (QUERY_NAME, None, {}, "abc", ERROR_MESSAGES['NOT_A_NUMBER'].format("abc")),
-    (QUERY_NAME, None, {}, -1, ERROR_MESSAGES['INVALID_TIMEOUT'].format(-1)),
-    (QUERY_NAME, "query ClusterLocationQuery()", {}, 10, ERROR_MESSAGES['INVALID_RAW_QUERY'])
-])
-def test_query_raw_when_invalid_values_are_provided(client, query_name, raw_query, variables, timeout, err_msg):
-    """ Test case scenario when invalid values are provided """
-    from rubrik_polaris.common.connection import _query_raw
-
-    with pytest.raises(Exception) as e:
-        _query_raw(client, query_name=query_name, raw_query=raw_query, variables=variables, timeout=timeout)
-
-    assert str(e.value) == err_msg
-
-
 def test_query_raw_when_valid_values_are_provided(requests_mock, client):
     """ Test case scenario when valid values are provided """
     from rubrik_polaris.common.connection import _query_raw
@@ -31,13 +16,13 @@ def test_query_raw_when_valid_values_are_provided(requests_mock, client):
     )
 
     requests_mock.post(BASE_URL + "/graphql", json=expected_response)
-    response = _query_raw(client, query_name=QUERY_NAME, variables={})
+    response = _query_raw(client, raw_query=None, operation_name=None, variables={}, timeout=60)
 
     assert response == expected_response
 
     graphql_file = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_data/raw_query.graphql"), 'r')\
         .read()
     raw_query = """{}""".format(graphql_file)
-    response = _query_raw(client, query_name=QUERY_NAME, variables={}, raw_query=raw_query)
+    response = _query_raw(client, raw_query=raw_query, operation_name=None, variables={}, timeout=60)
 
     assert response == expected_response
