574e1894a7713aeee351324f06a381179e127b98
DamaniN <37876601+DamaniN@users.noreply.github.com>
2024-08-19 22:45:05 -0700

    Support updating PCR config on every run.

--- sample/pcr-aws/pcr.py
+++ sample/pcr-aws/pcr.py
@@ -15,13 +15,11 @@ pp = pprint.PrettyPrinter(indent=2)
 #logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-a', '--awsaccountid', dest='awsAccountId', help="AWS Account ID used to download images from Rubrik ECR", default=None)
+parser.add_argument('-a', '--awsAccountid', dest='awsAccountId', help="AWS Account ID used to download images from Rubrik ECR", default=None,required=True)
 parser.add_argument('-d', '--domain', dest='domain', help="Polaris Domain", default=None)
-parser.add_argument('-e', '--exocomputeid', dest='exocomputeAccountId', help="Exocompute Account ID in Polaris/RSC", default=None)
 parser.add_argument('-k', '--keyfile', dest='json_keyfile', help="JSON Keyfile", default=None)
 parser.add_argument('-p', '--password', dest='password', help="Polaris Password", default=None)
 parser.add_argument('-r', '--root', dest='root_domain', help="Polaris Root Domain", default=None)
-parser.add_argument('-s', '--setprivatecontainercegistry', action="store_true", dest='setPcr', help="Sets the Private Container Registry", default=None)
 parser.add_argument('-u', '--username', dest='username', help="Polaris UserName", default=None)
 parser.add_argument('-v', '--verbose', help="Be verbose", action="store_const", dest="loglevel", const=logging.INFO)
 parser.add_argument('--debug', help="Print lots of debugging statements", action="store_const", dest="loglevel", const=logging.DEBUG, default=logging.WARNING)
@@ -35,9 +33,6 @@ args = parser.parse_args()
 pcrFqdn = args.pcrFqdn
 
 logging.basicConfig(level=args.loglevel)
-
-if args.setPcr and not (args.exocomputeAccountId and args.exocomputeAccountId):
-    parser.error('--setprivatecontainercegistry specified, however --awsaccountid or --exocomputeid not specified.')
 
 if args.pcrAuth == "PWD" and not (args.pcrPassword and args.pcrUsername):
     parser.error('Username/Password authentication to private container registry specified, however, --pcrPassword or --pcrUsername not specified.')
@@ -60,26 +55,46 @@ except Exception as err:
     sys.exit(1)
 
 # Set Private Container Registry (white list AWS account to download images)
-if args.setPcr:
-  variables = {
-    "input": {
-      "exocomputeAccountId": args.exocomputeAccountId,
-      "registryUrl": pcrFqdn,
-      "pcrAwsImagePullDetails": {
-        "awsNativeId": args.awsAccountId
+
+variables = {
+"awsNativeAccountIdOrNamePrefix": ""
+}
+
+try:
+    allAwsExocomputeConfigs = rubrik._query_raw(raw_query='query ($awsNativeAccountIdOrNamePrefix: String!) {allAwsExocomputeConfigs(awsNativeAccountIdOrNamePrefix: $awsNativeAccountIdOrNamePrefix) {awsCloudAccount {id nativeId accountName message seamlessFlowEnabled cloudType}}}',
+                                      operation_name=None,
+                                      variables=variables,
+                                      timeout=60)
+except Exception as err:
+    print("Error: Unable to retrieve the AWS account details.")
+    print(err)
+    sys.exit(1)
+
+logging.debug(json.dumps(allAwsExocomputeConfigs, indent=2))
+
+# Make sure that all repos are authorized to pull images from Rubrik ECR.
+for awsCloudAccount in allAwsExocomputeConfigs['data']['allAwsExocomputeConfigs']:
+  if awsCloudAccount['awsCloudAccount']['nativeId'] == args.awsAccountId:
+    print("Setting private container registry for AWS account " + awsCloudAccount['awsCloudAccount']['accountName'])
+    variables = {
+      "input": {
+        "exocomputeAccountId": awsCloudAccount['awsCloudAccount']['id'],
+        "registryUrl": pcrFqdn,
+        "pcrAwsImagePullDetails": {
+          "awsNativeId": args.awsAccountId
+        }
       }
     }
-  }
 
-  try:
-      setPrivateContainerRegistry = rubrik._query_raw(raw_query='mutation SetPrivateContainerRegistry($input: SetPrivateContainerRegistryInput!) {setPrivateContainerRegistry(input: $input)}',
-                                        operation_name=None,
-                                        variables=variables,
-                                        timeout=60)
-  except Exception as err:
-      print("Error: Unable to retrieve set the private container registry.")
-      print(err)
-      sys.exit(1)
+    try:
+        setPrivateContainerRegistry = rubrik._query_raw(raw_query='mutation SetPrivateContainerRegistry($input: SetPrivateContainerRegistryInput!) {setPrivateContainerRegistry(input: $input)}',
+                                          operation_name=None,
+                                          variables=variables,
+                                          timeout=60)
+    except Exception as err:
+        print("Error: Unable to set the private container registry.")
+        print(err)
+        sys.exit(1)
 
 # Login to AWS ECR
 
